0fb163facc0e402bd2f572c71b84524649a396b1
babilong <1070324761@qq.com>
2024-07-27 11:38:25 +0800

    GitHubTest项目第一次提交

---
+++ cmake-build-debug/CMakeFiles/GitHubTest.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/GitHubTest.dir/main.cpp.obj"
+  "GitHubTest.exe"
+  "GitHubTest.exe.manifest"
+  "GitHubTest.pdb"
+  "libGitHubTest.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/GitHubTest.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
